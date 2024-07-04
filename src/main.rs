92b23ecfdbe0151421ca92caabcd8aa0982ac257
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-07-04 15:12:15 +0200

    stash

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 #![no_main]
 #![no_std]
 
-use core::panic::PanicInfo;
-mod consts;
-use consts::NUMS;
+use panic_halt as _;
+
+mod assets;
+use assets::numbers::NUMS;
 
 use cortex_m_rt::entry;
-use embedded_hal::digital::{InputPin, OutputPin};
-use microbit::{
-    board::Board,
-    display::blocking::Display, 
-    hal::Timer};
-
-#[panic_handler]
-fn panic(_i: &PanicInfo) -> !{
-    loop {}
-}
+use embedded_hal::digital::{InputPin};
+use microbit::Board;
+use microbit::hal::Timer;
+use microbit::display::blocking::Display;
 
 #[entry]
 fn main() -> ! {
-    let board = Board::take().unwrap();
+    let mut board = Board::take().unwrap();
     let mut display = Display::new(board.display_pins);
     let mut timer = Timer::new(board.TIMER0);
 
     let mut index_num: i8 = 0;
 
-    loop {
-        mma8x5x
+loop {
+        
+        index_num += 1;
+        // loop indexing when overflow
+        if index_num > 25 { index_num = 0; }
+        if index_num < 0 { index_num = 25; }
+
+        display.show(&mut timer, NUMS[index_num as usize], 150);
+        display.clear()
     }
 }
