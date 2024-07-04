d3148a1602e91efcbb194e97795e912026028155
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-07-04 12:02:21 +0200

    counting test loop

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
+use core::panic::PanicInfo;
+mod consts;
+use consts::NUMS;
 
 use cortex_m_rt::entry;
-use embedded_hal::{delay::DelayNs, digital::OutputPin};
-use microbit::{board::Board, hal::timer::Timer};
+use embedded_hal::digital::{InputPin, OutputPin};
+use microbit::{board::Board, display::blocking::Display, hal::Timer};
+
+#[panic_handler]
+fn panic(_i: &PanicInfo) -> !{
+    loop {}
+}
 
 #[entry]
 fn main() -> ! {
     let mut board = Board::take().unwrap();
-
+    let mut display = Display::new(board.display_pins);
     let mut timer = Timer::new(board.TIMER0);
 
-    let _ = board.display_pins.col3.set_low();
-    let mut row1 = board.display_pins.row2;
+    let mut index_num: i8 = 0;
 
     loop {
-        let _ = row1.set_low();
-        timer.delay_ms(1_000);
-        let _ = row1.set_high();
-        timer.delay_ms(1_000);
+        //if let Ok(true) = board.buttons.button_a.is_high() {
+            //index_num += 1;
+        //}
+        //if let Ok(true) = board.buttons.button_b.is_high() {
+            //index_num -= 1;
+        //}
+        // prefent out of bounds index
+        index_num += 1;
+        if index_num > 25 {index_num = 0};
+        if index_num < 0 {index_num = 25};
+
+        display.show(&mut timer, NUMS[index_num as usize], 500)
     }
 }
