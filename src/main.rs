96f868d479ac434e30b17a663aeab56c18fc6ebf
Quantum-Coder826 <b.veldthuis@gmail.com>
2024-07-04 09:30:51 +0200

    added hal-blinky form micribit crate

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-use cortex_m::asm::nop;
-use cortex_m_rt::entry;
 use panic_halt as _;
-//use rtt_target::{rprintln, rtt_init_print};
+
+use cortex_m_rt::entry;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board::Board, hal::timer::Timer};
 
 #[entry]
 fn main() -> ! {
-    let mut x: usize = 0;
+    let mut board = Board::take().unwrap();
+
+    let mut timer = Timer::new(board.TIMER0);
+
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
+
     loop {
-        x += 1;
-        for _ in 0..x {
-            nop()
-        };
+        let _ = row1.set_low();
+        timer.delay_ms(1_000);
+        let _ = row1.set_high();
+        timer.delay_ms(1_000);
     }
 }
